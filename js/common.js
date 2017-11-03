c09415990b7b7482f76dca0cc70dc0a45624a3bf
cma2819 <yamanaka-team@hotmail.co.jp>
2017-11-03 14:40:15 +0900

    Update header layout and message impelments

---
+++ js/common.js
@@ -0,0 +1,25 @@
+﻿
+/*
+	共通JavaScript.
+*/
+
+/*
+	ページ表示時遅延
+*/
+function show() {
+	// $("body").css("visibility","visible");
+	$('body').fadeIn();
+}
+
+window.addEventListener("load", function(){
+  show();
+}, false);
+
+/*
+	メッセージ消去
+*/
+function outMessage() {
+	var messageid = '#message'
+	var element = $(messageid);
+	element.fadeOut(500);
+}
